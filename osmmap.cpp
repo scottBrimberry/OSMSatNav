1e663a08ab24389ae0986fe55461599d241eef49
Gary Steinert <gary.steinert@gmail.com>
2009-07-16 19:57:23 +0100

    Start implementing Equirectangular projection - plate carree

--- osmmap.cpp
+++ osmmap.cpp
@@ -1,10 +1,25 @@
 #include "osmmap.h"
 
+#include "mercator.h"
 #include "osmrelation.h"
 
-OSMMap::OSMMap()
-{
-}
+OSMMap::OSMMap() {}
+
+//TODO: Check cnter is needed
+void OSMMap::setMinLat( double lat ) { m_minlat = lat; m_miny = Mercator::LatToY( lat ); }
+void OSMMap::setMaxLat( double lat ) { m_maxlat = lat; m_maxy = Mercator::LatToY( lat ); }
+void OSMMap::setMinLon( double lon ) { m_minlon = lon; m_minx = Mercator::LonToX( lon ); }
+void OSMMap::setMaxLon( double lon ) { m_maxlon = lon; m_maxx = Mercator::LonToX( lon ); }
+
+double OSMMap::minLat() { return m_minlat; }
+double OSMMap::maxLat() { return m_maxlat; }
+double OSMMap::minLon() { return m_minlon; }
+double OSMMap::maxLon() { return m_maxlon; }
+
+double OSMMap::minX() { return m_minx; }
+double OSMMap::maxX() { return m_maxx; }
+double OSMMap::minY() { return m_miny; }
+double OSMMap::maxY() { return m_maxy; }
 
 void OSMMap::addNode( OSMNode node ) 
 { 
@@ -90,6 +105,6 @@ void OSMMap::paint( QPainter* painter )
   foreach( OSMWay i, ways() )
   {
     //TODO: The mim lat and lon need to be part of the object read from the XML file
-    i.paint( painter, 52.69855, -2.76612 );
+    i.paint( painter, 52.622, -2.5293 );
   }
 }
