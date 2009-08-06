a328c0ed9fda237ddd29a6568b6ab872c3b53a96
Gary Steinert <gary@77.96.18.123>
2009-08-06 20:50:08 +0100

    Fix problems creating Tag table.
    Turns out Key is a reserved word. To make it easier (rather than using
    quotes every time i need to use it) changed the Key field to "Tag".

--- database.cpp
+++ database.cpp
@@ -36,16 +36,16 @@ bool OSMDatabase::open( QString host, QString name, QString user, QString pass )
       }
   
       //Create tables
-      query.exec( "CREATE TABLE Nodes( Id int, Longitude float, Latitude float )" );
-      query.exec( "CREATE TABLE Ways( Id int )" );
-      query.exec( "CREATE TABLE Relations( Id int )" );
-      query.exec( "CREATE TABLE Tags ( Id int, Key varchar(255), Value varchar(255) )" );
-      query.exec( "CREATE TABLE NodeTags ( Node int, Tag int )" );
-      query.exec( "CREATE TABLE WayNodes ( Way int, Node int )" );
-      query.exec( "CREATE TABLE WayTags ( Way int, Tag int )" );
-      query.exec( "CREATE TABLE RelationWays ( Relation int, Way int )" );
-      query.exec( "CREATE TABLE RelationNodes ( Relation int, Node int )" );
-      query.exec( "CREATE TABLE RelationTags ( Relation int, Tag int )" );
+      query.exec( "CREATE TABLE Nodes ( Id INT, Longitude FLOAT, Latitude FLOAT )" );
+      query.exec( "CREATE TABLE Ways ( Id INT )" );
+      query.exec( "CREATE TABLE Relations ( Id INT )" );
+      query.exec( "CREATE TABLE Tags ( Id INT, Tag VARCHAR(255), Value VARCHAR(255) )" );
+      query.exec( "CREATE TABLE NodeTags ( Node INT, Tag INT )" );
+      query.exec( "CREATE TABLE WayNodes ( Way INT, Node INT )" );
+      query.exec( "CREATE TABLE WayTags ( Way INT, Tag INT )" );
+      query.exec( "CREATE TABLE RelationWays ( Relation INT, Way INT )" );
+      query.exec( "CREATE TABLE RelationNodes ( Relation INT, Node INT )" );
+      query.exec( "CREATE TABLE RelationTags ( Relation INT, Tag INT )" );
     }
     
     return false;
